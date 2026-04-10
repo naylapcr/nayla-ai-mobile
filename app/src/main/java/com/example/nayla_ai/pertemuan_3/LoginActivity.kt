a43106f382d735b3055cb2671cf231be770d4e74
naylapcr <nayla24si@mahasiswa.pcr.ac.id>
2026-04-11 00:55:42 +0700

    p4

--- app/src/main/java/com/example/nayla_ai/pertemuan_3/LoginActivity.kt
+++ app/src/main/java/com/example/nayla_ai/pertemuan_3/LoginActivity.kt
@@ -11,21 +11,13 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inisialisasi ViewBinding
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-            val nama = binding.etUsername.text.toString()
-
-            if (nama.isEmpty()) {
-                binding.etUsername.error = "Nama tidak boleh kosong!"
-            } else {
-                // PERTEMUAN 4: Intent Eksplisit + Kirim Data
-                val intent = Intent(this, WelcomeActivity::class.java)
-                intent.putExtra("NAMA_USER", nama) // "NAMA_USER" adalah kunci/labelnya
-                startActivity(intent)
-            }
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
         }
-
     }
 }
